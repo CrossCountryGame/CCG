4eee9e2fc1fabd9588a2474ddab1568b953badb3
cris1314 <cristopherjrivasc@gmail.com>
2017-02-27 00:40:34 -0430

    Perfomance Update

--- Assets/UIMainMenu.cs
+++ Assets/UIMainMenu.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using AppAdvisory.social;
 
 public class UIMainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		LeaderboardManager.Init ();
+
 	}
 	public void PlayButton(){
 		Manager.mng.PlayGame();
 	}
 	public void LeaderBoardButton(){
-		Manager.mng.ShowLeaderBoardAndroid ();
+		LeaderboardManager.ShowLeaderboardUI ();
 	}
 	// Update is called once per frame
 	void Update () {
